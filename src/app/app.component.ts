f184c1d614ec9383f880dd9368839caf1bb4f54a
umangsuthar <umangsuthar97@gmail.com>
2017-10-11 14:04:58 +0530

    Item Add Remove APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items = ["java","angular","vue"];
+  inputItem = "";
+
+        pushItem = function(){
+            if ( this.inputItem != "")
+            {
+              this.items.push(this.inputItem);
+              this.inputItem = "";
+            }
+          }
+        popItem = function(index){
+            this.items.splice(index,1);
+        }
 }
